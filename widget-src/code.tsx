d10a48f69e7df1d177140f665397fb445c6e81a9
toyamani <neotyhdpc@gmail.com>
2023-10-31 20:11:29 +0900

    feat: add left line

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -131,13 +131,31 @@ function Widget() {
   );
 
   return (
-    <AutoLayout>
-      <Line
-        length={200}
-        strokeWidth={20}
-        stroke={tag.color}
-        positioning="absolute"
-      />
+    <AutoLayout horizontalAlignItems={"center"} verticalAlignItems="center">
+      <AutoLayout
+        width={200}
+        height={50}
+        direction="horizontal"
+        verticalAlignItems="center"
+        horizontalAlignItems="center"
+        y={50}
+      >
+        <Ellipse
+          fill={color}
+          width={15}
+          height={15}
+          y={15}
+          positioning="absolute"
+        />
+        <Line
+          length={200}
+          strokeWidth={5}
+          stroke={color}
+          y={25}
+          x={10}
+          positioning="absolute"
+        />
+      </AutoLayout>
       <AutoLayout
         direction={"vertical"}
         verticalAlignItems={"center"}
